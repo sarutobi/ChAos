4b593788d31ffe0ff3b3d7a175fa0242250ea055
Valery A. Ilychev <ilychev@gmail.com>
2013-02-05 15:32:24 +0400

    Include challenge urls

--- ChAos/urls.py
+++ ChAos/urls.py
@@ -8,10 +8,5 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'ChAos.views.home', name='home'),
     # url(r'^ChAos/', include('ChAos.foo.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+    url('^challenge/', include('challenge.urls'))
 )

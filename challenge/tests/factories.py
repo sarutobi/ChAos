4302fb5079eb6d16c71c565f61e73266389858a7
Valery A. Ilychev <ilychev@gmail.com>
2013-02-06 16:50:12 +0400

    Add activity factory

--- challenge/tests/factories.py
+++ challenge/tests/factories.py
@@ -17,3 +17,13 @@ class ChallengeFactory(factory.Factory):
     slug = factory.LazyAttribute(lambda a: "%s_slug" % a.title.lower())
     start_at = datetime(2012, 1, 1, 8, 0, 0)
     end_at = datetime(2012, 2, 1, 8, 0, 0)
+
+
+class ActivityFactory(factory.Factory):
+    FACTORY_FOR = Activity
+
+    title = factory.Sequence(lambda n: "Activity %s" % n)
+    description = factory.LazyAttribute(lambda a: a.title * 3)
+    reward = random.randint(3, 10)
+    reward_cost = random.randint(2, 20)
+    reward_cost_type = random.randint(1, 4)

e5163f27330e70a8515de6db9e2a6f6d9e3a3ff4
Valery A. Ilychev <ilychev@gmail.com>
2013-02-08 11:19:32 +0400

    Add tests for activity form required data

--- challenge/tests/test_forms.py
+++ challenge/tests/test_forms.py
@@ -90,3 +90,41 @@ class ActivityFormTest(unittest.TestCase):
         self.assertIsNotNone(form)
         self.assertTrue(form.is_bound)
         self.assertTrue(form.is_valid())
+
+
+class ActivityFormRequiredFields(unittest.TestCase):
+
+    def setUp(self):
+        self.challenge = ChallengeFactory()
+        self.user = UserFactory()
+        self.activity = ActivityFactory.attributes()
+        self.activity['challenge'] = self.challenge.pk
+        self.activity['creator'] = self.user.pk
+
+    def tearDown(self):
+        self.activity = None
+        self.user.delete()
+        self.challenge.delete()
+
+    def test_correct(self):
+        form = ActivityForm(self.activity)
+        self.assertTrue(form.is_bound)
+        self.assertTrue(form.is_valid())
+
+    def test_lost_title(self):
+        self.activity['title'] = ''
+        form = ActivityForm(self.activity)
+        self.assertTrue(form.is_bound)
+        self.assertFalse(form.is_valid())
+
+    def test_lost_reward(self):
+        self.activity['reward'] = ''
+        form = ActivityForm(self.activity)
+        self.assertTrue(form.is_bound)
+        self.assertFalse(form.is_valid())
+
+    def test_lost_reward_cost_type(self):
+        self.activity['reward_cost_type'] = ''
+        form = ActivityForm(self.activity)
+        self.assertTrue(form.is_bound)
+        self.assertFalse(form.is_valid())

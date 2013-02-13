18097fd2ffb39e0de1437d3d9db6533ec27240b4
Valery A. Ilychev <ilychev@gmail.com>
2013-02-13 15:35:53 +0400

    Clean challenge view

--- challenge/views.py
+++ challenge/views.py
@@ -1,17 +1,11 @@
 # -*- coding: utf-8 -*-
 
 import logging
-from random import choice
 
 from django.contrib.auth import logout
-from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
-from django.contrib.auth.models import User
-from django.template.response import TemplateResponse
 from django.http import HttpResponseRedirect
 from django.views.generic.detail import DetailView
 from django.views.generic.list import ListView
-from django.views.generic.edit import CreateView, UpdateView
-from django.shortcuts import redirect
 
 from .models import Challenge, Activity
 
@@ -42,88 +36,3 @@ class ActivityView(DetailView):
     model = Activity
     template = "activity_detail.html"
     context_object_name = "activity"
-
-#class CreateUser(CreateView):
-#    '''Register form'''
-#    form_class = UserCreationForm
-#    template_name = "register_form.html"
-#    success_url = "/"
-
-
-#class CreateTask(CreateView):
-#    '''
-#    Task creation functionality.
-#    '''
-#    form_class = TaskForm
-#    template_name = "application_form.html"
-#    success_url = "/apps/created"
-#    initial = {}
-#
-#    def get(self, request, *args, **kwargs):
-#        self.initial['application'] =  kwargs.get('pk')
-#        return super(CreateView, self).get(request, *args, **kwargs)
-
-#    def form_valid(self, form):
-#        task = form.save(commit=False)
-#        task.user_id = self.request.user.pk
-#        task.status = 0
-#        task.save()
-#        return redirect(self.success_url)
-
-
-#class AppTasksList(ListView):
-#    '''Show tasks, linked to specific application'''
-#    template_name = 'task_list.haml'
-#    context_object_name = 'tasks'
-#
-#    def get_context_data(self, **kwargs):
-#        context = super(AppTasksList, self).get_context_data(**kwargs)
-#        context['app'] = self.app
-#        return context
-#
-#    def get_queryset(self):
-#        self.app = Application.objects.get(slug=self.kwargs.get('slug'))
-#        return Task.objects.filter(application=self.app)
-#
-#
-#class UpdateApp(UpdateView):
-#    ''' Update application data view'''
-#    model = Application
-#    form_class = ApplicationForm
-#    template_name = "application_form.html"
-#    success_url = "/apps/created"
-#
-#
-#class AppDetails(DetailView):
-#    '''Application details view'''
-#    model = Application
-#    template_name = "application_details.html"
-#    context_object_name = "app"
-#
-#
-#class UserProfile(DetailView):
-#    '''Own user profile view'''
-#    model = User
-#    template_name="user_profile.html"
-#
-#    def get_object(self):
-#        return self.request.user
-#
-#
-#class UserApps(ListView):
-#    '''Self created applications'''
-#    template_name="user_applications.html"
-#    context_object_name = 'apps'
-#
-#    def get_queryset(self):
-#        return Application.objects.filter(user_id=self.request.user.id)
-#
-#
-#class BaseTaskView(DetailView):
-#    '''Show task for user and get an answer'''
-#    template_name = "task_skeleton.html"
-#
-#    def get_object(self):
-#        task = TaskRun.objects.exclude(user=self.request.user)
-#        task_id = Task.objects.exclude(id__in=task).values('id')
-#        return Task.objects.select_related('application').get(id=choice(task_id)['id'])

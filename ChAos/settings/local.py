97ae601d423ed46bb657b0358fc9e8ef75ba0b7c
Valery A. Ilychev <ilychev@gmail.com>
2013-02-13 09:28:25 +0400

    Moved common local settings from dev_sarutobi to local

--- ChAos/settings/local.py
+++ ChAos/settings/local.py
@@ -7,3 +7,23 @@ ADMINS = (
     # ('Your Name', 'your_email@example.com'),
 )
 MANAGERS = ADMINS
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'chaos',
+        'USER': 'chaos',
+        'PASSWORD': 'chaos',
+        'HOST': 'localhost',
+        'PORT': '',
+    }
+}
+
+TIME_ZONE = 'Europe/Moscow'
+LANGUAGE_CODE = 'ru'
+
+INSTALLED_APPS += (
+    'debug_toolbar',
+)
+MIDDLEWARE_CLASSES += ('debug_toolbar.middleware.DebugToolbarMiddleware',)
+INTERNAL_IPS = ('127.0.0.1')

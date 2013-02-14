d47386da3686aa51e1c7cb8a7ca27c5e6b287028
Valery A. Ilychev <ilychev@gmail.com>
2013-02-14 15:17:23 +0400

    Small fixes
    Fixed base template content.
    Fixed challenge view url
    Added creation and edit challenge views to url

--- challenge/urls.py
+++ challenge/urls.py
@@ -2,12 +2,16 @@
 
 from django.conf.urls import patterns, include, url
 
-from .views import ChallengeList, ChallengeView, ActivityView
+from .views import (
+    ChallengeList, ChallengeView, ChallengeCreation, ChallengeEdit,
+    ActivityView
+)
 
 urlpatterns = patterns(
     "",
     url(r'^$', ChallengeList.as_view(), name="challenges_list"),
-    url(r'^(?P<slug>[\w_-]+$)', ChallengeView.as_view(),
+    url(r'^create$', ChallengeCreation.as_view(), name='challenge_create'),
+    url(r'^view/(?P<slug>[\w_-]+$)', ChallengeView.as_view(),
         name="challenge_view"),
     url(r'^activity/(?P<id>\d+$)', ActivityView.as_view(),
         name="activity_view"),
